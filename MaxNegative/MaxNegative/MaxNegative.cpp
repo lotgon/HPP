813581fc53cd9ea0d6dba5a851a59f0e8dc94b5e
lotgon <andrei1pazniak@gmail.com>
2015-04-21 17:56:54 +0300

    da

--- MaxNegative/MaxNegative/MaxNegative.cpp
+++ MaxNegative/MaxNegative/MaxNegative.cpp
@@ -9,8 +9,9 @@
 #include <amp.h>
 #include "AMPHelper.h"
 #include <chrono>
-
-#define TILE_SIZE 128
+#include <ppltasks.h>
+#include <array>
+#define TILE_SIZE 256
 
 template<typename TimeT = std::chrono::microseconds,
 	typename ClockT = std::chrono::high_resolution_clock,
@@ -30,70 +31,131 @@ public:
 };
 
 
-void CalculateDrawdown(concurrency::array_view<float, 1> P, float tp, std::vector<float> &outDrawdown, std::vector<int> &outDuration)
+concurrency::completion_future CalculateDrawdown(concurrency::array_view<float, 1> P, float tp, concurrency::array_view<float, 1> &D, concurrency::array_view<int, 1> &duration)
 {
 	int sz = P.extent[0];
-	outDrawdown.resize(sz);
-	outDuration.resize(sz);
-
-
-	concurrency::array_view<float, 1> D(sz, &outDrawdown[0]);
-	concurrency::array_view<int, 1> duration(sz, &outDuration[0]);
 	D.discard_data();
 	duration.discard_data();
+	concurrency::tiled_extent<TILE_SIZE> compute_domain = D.get_extent().tile<TILE_SIZE>().pad();
 
-	parallel_for_each(D.extent.tile<TILE_SIZE>(), [=](concurrency::tiled_index<TILE_SIZE> t_idx) restrict(amp)
+	//std::wcout << "Starting GPU Calc"<<std::endl;
+	parallel_for_each(compute_domain, [=](concurrency::tiled_index<TILE_SIZE> t_idx) restrict(amp)
 	{
+		
+		float threshold = P[t_idx.global] + tp;
+		float maxDrawdown = 0;
+		float open = P[t_idx.global];
 
-		float threshold = P[t_idx.global];
 		tile_static float my_tile[TILE_SIZE];
 		tile_static int calculatedCount;
 
-		for (int stride=0; calculatedCount < TILE_SIZE; stride++)
+		int stride = 0;
+		bool isPointCalculated = false;
+
+		calculatedCount = 0;
+		t_idx.barrier.wait();
+
+		for (; calculatedCount < TILE_SIZE; stride++)
 		{
-			my_tile[t_idx.local[0]] = P[t_idx.global[0] + stride*TILE_SIZE];
+			if (t_idx.global[0] + stride*TILE_SIZE < sz)
+				my_tile[t_idx.local[0]] = P[t_idx.global + stride*TILE_SIZE];
+			else
+				my_tile[t_idx.local[0]] = 0;
+
 			t_idx.barrier.wait();
-			for (int i =0; i < TILE_SIZE; i++)
+
+			if (!isPointCalculated)
 			{
-				if (my_tile[i] >= threshold)
+				for (int i = stride == 0 ? t_idx.local[0] : 0; i < TILE_SIZE; i++)
 				{
-					D[t_idx.global] = 0;
-					calculatedCount++;
+					if (stride*TILE_SIZE + i + (t_idx.tile[0]) * TILE_SIZE >= sz)
+					{
+						isPointCalculated = true;
+						concurrency::atomic_fetch_inc(&calculatedCount);
+						D[t_idx.global] = maxDrawdown;
+						duration[t_idx.global] = 0;
+						break;
+					}
+
+					if (maxDrawdown < open - my_tile[i])
+						maxDrawdown = open - my_tile[i];
+					if (my_tile[i] >= threshold)
+					{
+						duration[t_idx.global] = stride*TILE_SIZE + i - t_idx.local[0];
+						D[t_idx.global] = maxDrawdown;
+						isPointCalculated = true;
+						concurrency::atomic_fetch_inc(&calculatedCount);
+						break;
+					}
 				}
 			}
+			t_idx.barrier.wait();
+
 		}
 	});
+	concurrency::completion_future complEvent =  D.synchronize_async();
+	//std::wcout << "Finish GPU Calc" << std::endl;
+	return complEvent;
 }
-void CalculateAll(std::vector<float> &prices)
+void CalculateAll(std::vector<float> &prices, std::vector<std::string> &vectorDates)
 {
 	int barCount = prices.size();
-	std::ofstream outputFile("c:\\projects\\CPlusPlus\\MaxNegative\\Data\\silverDayResult.csv");
+	std::ofstream outputFile("..//Data//silverDayResult.csv");
 	outputFile << "Date, Tp, Drawdown,BarDuration\n";
 
 	concurrency::array_view<float, 1> P(barCount, &prices[0]);
+	concurrency::array_view<float, 1> P2(barCount, &prices[0]);
+	concurrency::array_view<float, 1> P3(barCount, &prices[0]);
 
-	float currTp = 0.5, maxTp = 2, stepTp = 0.0001;
+	int number = 10;
+
+	std::vector<concurrency::completion_future> arSync;
+	int i = 0;
+	float currTp = 0.5, maxTp = 2., stepTp = 10.01;
+	float sum = 0;
 	do
 	{
-		std::vector<float> vectorDrawdown;
-		std::vector<int> vectorDuration;
+		std::vector<float> vectorDrawdown(barCount);
+		std::vector<int> vectorDuration(barCount);
+		concurrency::array_view<float, 1> D(vectorDrawdown);
+		concurrency::array_view<int, 1> duration(vectorDuration);
 
-		CalculateDrawdown(P, currTp, vectorDrawdown, vectorDuration);
+		std::vector<float> vectorDrawdown2(barCount);
+		std::vector<int> vectorDuration2(barCount);
+		concurrency::array_view<float, 1> D2(vectorDrawdown);
+		concurrency::array_view<int, 1> duration2(vectorDuration);
+
+		std::vector<float> vectorDrawdown3(barCount);
+		std::vector<int> vectorDuration3(barCount);
+		concurrency::array_view<float, 1> D3(vectorDrawdown);
+		concurrency::array_view<int, 1> duration3(vectorDuration);
+
+		Stopwatch<> sw0;
+		sw0.start();
+		arSync.push_back(CalculateDrawdown(P, currTp, D, duration));
+		arSync.push_back(CalculateDrawdown(P2, currTp+0.01, D2, duration2));
+		arSync.push_back(CalculateDrawdown(P3, currTp + 0.01, D3, duration3));
+
 		//for (int i = 0; i < barCount; i++)
 		//outputFile << vectorDates[i] << ", " << currTp<<", "<< vectorDrawdown[i] << ", " << vectorDuration[i] << "\n";
+		sw0.stop();
+		for (const auto &elem : arSync)
+			elem.get();
+		std::wcout << " Iteration Execution time is " << sw0.elapsed() / 1000 << " milliseconds";
 
 		currTp += stepTp;
 	} while (currTp < maxTp);
+
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	CAMPHelper::default_properties();
 	CAMPHelper::list_all_accelerators();
-	std::ifstream ss("c:\\projects\\CPlusPlus\\MaxNegative\\Data\\silverDay.csv");
+	std::ifstream ss("..//Data//silverDay.csv");
 
 	bool result;
-	result = true;// CAMPHelper::PickEmulatedAccelerator();
+	result = true; //CAMPHelper::PickEmulatedAccelerator();
 	//std::wstring ws = L"direct3d\\warp";//CPU accelerator";
 	//std::wstring ws = L"cpu";
 	//result = CAMPHelper::PickAccelerator(ws);
@@ -116,15 +178,12 @@ int _tmain(int argc, _TCHAR* argv[])
 		vectorPrices.push_back(std::stof(price));
 		vectorDates.push_back(date1);
 	}
-
 	
-
-
 
 	Stopwatch<> sw0;
 	sw0.start();
 	
-	CalculateAll(vectorPrices);
+	CalculateAll(vectorPrices, vectorDates);
 
 	sw0.stop();
 	std::wcout << " Execution time is " << sw0.elapsed()/1000 << " milliseconds";
